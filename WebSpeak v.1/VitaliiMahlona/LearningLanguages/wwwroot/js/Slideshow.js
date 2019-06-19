a10cd9914780775c3d13dc69b4c1cb163a6d4891
MahlonaVitalii <maglona2013@gmail.com>
2019-06-20 00:50:47 +0300

    modified slideshow

---
+++ WebSpeak v.1/VitaliiMahlona/LearningLanguages/wwwroot/js/Slideshow.js
@@ -0,0 +1,49 @@
+﻿$('#slideshow').on('slide.bs.carousel', function (e) {
+    var listNames = [];
+    var duration = 0;
+
+    listNames.push("sound");
+    listNames.push("pronounceLearn");
+    listNames.push("pronounceNative");
+
+    var listAudio = GetList(listNames);
+
+    play(listAudio);
+
+    for (let i = 0; i < listAudio.length; ++i) {
+        duration += listAudio[i].duration;
+    }
+
+    var slideshow = document.getElementsByClassName('carousel-item')
+
+    for (let i = 0; i < slideshow.length; i++) {
+        slideshow[i].style.transitionDelay = duration + "s";
+    }
+})
+
+function play(list) {
+    for (let i = 0; i < list.length; i++) {
+        if (i === 0) {
+            list[i].play()
+        } else {
+            list[i - 1].addEventListener('ended', function () {
+                list[i].play()
+            })
+        }
+    }
+}
+
+function GetList(listNames) {
+    var listAudio = [];
+
+    for (let i = 0; i < listNames.length; ++i) {
+        var soundId = $('#slideshow').find(`.active .carousel-caption audio.${listNames[i]}`).attr('id');
+
+        if (soundId != null) {
+            var sound = document.getElementById(soundId);
+            listAudio.push(sound);
+        }
+    }
+
+    return listAudio;
+}
---
+++ WebSpeak v.1/VitaliiMahlona/LearningLanguages/wwwroot/js/Tests.js
@@ -0,0 +1,6 @@
+﻿var select = document.querySelectorAll('#actionLink a');
+for (var i = 0; i < select.length; i++) {
+    select[i].className += "btn btn-outline-success";
+    select[select.length - 1].setAttribute("role", "button");
+    select[select.length - 1].setAttribute("aria-pressed", "true");
+}
