ea78f984a76e6df2c5f79b5bf05b2637a9d1ef8f
Vitalii_Rozbyiholova <rozbiygolova98@gmail.com>
2019-06-21 17:13:10 +0300

    Added some styles

--- WebSpeak v.1/Vitalii_Rozbyiholova/WebSpeak/wwwroot/js/site.js
+++ WebSpeak v.1/Vitalii_Rozbyiholova/WebSpeak/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('.item').click(function () {
+    let loc = $(this).find("a").attr("href");
+    if (loc) {
+        window.location = $(this).find("a").attr("href");
+    }    
+});
