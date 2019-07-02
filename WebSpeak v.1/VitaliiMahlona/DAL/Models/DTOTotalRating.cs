94866695fe6f78cd7e5c476e557dcff0e01b6062
MahlonaVitalii <maglona2013@gmail.com>
2019-07-02 22:35:42 +0300

    logic moved to server(rating and statistics)

---
+++ WebSpeak v.1/VitaliiMahlona/DAL/Models/DTOTotalRating.cs
@@ -0,0 +1,10 @@
+﻿namespace DAL.Models
+{
+    public class DTOTotalRating
+    {
+        public string Username { get; set; }
+        public int Total { get; set; }
+        public string Lang { get; set; }
+        public int Id { get; set; }
+    }
+}
